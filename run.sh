a7f4d11752abf7cfcfedf5bb551eeb02b753c1b1
Tobias Kuhn <kuhntobias@gmail.com>
2020-04-02 16:40:50 +0200

    Update script and add results

--- run.sh
+++ run.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-while read I; do
-  echo "LDF instance: $I"
-  time comunica-sparql $I -f queries/1-nanopub.rq
-done < ldf-instances.txt
+rm -f results.csv
+
+for Q in queries/*.rq; do
+  echo "Using query $Q"
+  QX=`echo $Q | sed -r 's/^queries\/(.*)\.rq$/\1/'`
+  while read I; do
+    echo "Trying LDF instance $I"
+    IX=`echo $I | sed -r 's/https?:\/\/([^\/]*)\/.*/\1/' | sed -r 's/[^0-9a-z]/-/g'`
+    mkdir -p output/$IX/$QX
+    (
+      time -p comunica-sparql $I -f $Q \
+        > output/$IX/$QX/query-results.json
+    ) \
+      2> output/$IX/$QX/time.txt
+    echo -n "$QX,$I," >> results.csv
+    cat output/$IX/$QX/time.txt | grep "real" | sed "s/real //" | tr -d \\n >> results.csv
+    echo -n "," >> results.csv
+    cat output/$IX/$QX/query-results.json | egrep "^{" | wc -l >> results.csv
+  done < ldf-instances.txt
+done
